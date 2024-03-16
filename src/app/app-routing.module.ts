abff2afb0f8f4c57f7e35458ed499b65170dfff8
Lucas Duarte <lucasduarte647@gmail.com>
2024-03-16 13:04:08 -0300

    Adjustment toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
   {
-    path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+    path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'file', loadChildren: () => import('./pages/file-view/file-view.module').then(m => m.FileViewModule)
--- src/app/pages/file-view/file-view-routing.module.ts
+++ src/app/pages/file-view/file-view-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FileViewComponent } from './file-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FileViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
