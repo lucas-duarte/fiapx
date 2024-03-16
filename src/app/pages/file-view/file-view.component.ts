d7273e8243b6c060f6596ea0de634f9611af89ee
Lucas Duarte <lucasduarte647@gmail.com>
2024-03-16 13:13:43 -0300

    file view

--- src/app/pages/file-view/file-view.component.ts
+++ src/app/pages/file-view/file-view.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { fadeInAnimation, fadeOutAnimation } from 'src/app/core/constants/animations';
+import { FileService } from 'src/app/core/services/file.service';
 
 @Component({
   selector: 'app-file-view',
   templateUrl: './file-view.component.html',
-  styleUrls: ['./file-view.component.scss']
+  styleUrls: ['./file-view.component.scss'],
+  animations: [fadeInAnimation, fadeOutAnimation]
 })
 export class FileViewComponent {
+
+constructor(private fileService: FileService){}
 
   displayButtonUpload: boolean = false;
   files: File[] = [];
